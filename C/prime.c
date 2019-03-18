6a549e5ac59d3bcd4aab67ae85c830a1d10f013d
Jeremy Gaudet <j_gaudet@live.concordia.ca>
2019-03-17 22:00:36 -0400

    develop prime

--- C/prime.c
+++ C/prime.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Prime
 {
@@ -7,6 +8,7 @@ struct Prime
 };
 
 void makePrimeList(struct Prime p);
+void swap(int* a, int* b);
 
 int main(int argc, char const *argv[])
 {
@@ -15,18 +17,33 @@ int main(int argc, char const *argv[])
     scanf("%d %d", &x, &y);
 
     printf("This is what you entered: %d, and %d.\nI will now display all intermediate prime numbers, inclusive:\n", x, y);
+    if (x > y)
+        swap(&x, &y);
     struct Prime myPrimes;
     myPrimes.x = x;
     myPrimes.y = y;
+    myPrimes.primelist = malloc(sizeof(int)*(y-x));
     
     makePrimeList(myPrimes);
 
 
 }
 
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 void makePrimeList(struct Prime p) {
-    int i = p.x;
-    for ( ; i < p.y; i++) {
-        
+    int t = p.x;
+    int size = p.y-p.x;
+    printf("this is x: %d, and this is y: %d\n", p.x,p.y);
+    for (int i = 0; i < size; i++) {
+        p.primelist[i] = t;
+        t++;
+    }
+    for (int i = 0; i < size; i++) {
+        printf("p.primeList[%d] = %d\n", i, p.primelist[i]);
     }
 }
